9226545cfeb452487ff1d12489cb87c8a9b11117
adarshdhital007 <adarsh7466@gmail.com>
2023-05-15 01:48:39 +0530

    Update main.ps1

--- TBC/main.ps1
+++ TBC/main.ps1
@@ -1,25 +1,14 @@
-# Set the base directory where the folders are located
-$baseDirectory = "E:\demo"
-
-# Set the names of the .cpp files to be added
-$cppFiles = @("1.cpp", "2.cpp", "3.cpp")  # Add more file names if needed
-
-# Get a list of folder names in the base directory
+$baseDirectory = "src"
+$cppFiles = @("", "", "")
 $folderNames = Get-ChildItem -Path $baseDirectory -Directory | Select-Object -ExpandProperty Name
 
-# Loop through each folder name
 foreach ($folderName in $folderNames) {
-    # Create the full path of the folder
     $folderPath = Join-Path -Path $baseDirectory -ChildPath $folderName
 
-    # Loop through each .cpp file name
     foreach ($cppFile in $cppFiles) {
-        # Create the full path of the .cpp file
         $cppFilePath = Join-Path -Path $folderPath -ChildPath $cppFile
 
-        # Check if the .cpp file already exists in the folder
         if (-not (Test-Path $cppFilePath -PathType Leaf)) {
-            # Create a new empty .cpp file
             New-Item -Path $cppFilePath -ItemType File | Out-Null
             Write-Host "Added $cppFile to $folderName"
         }
