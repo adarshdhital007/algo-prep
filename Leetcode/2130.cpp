d2ea9d2e38e1bb57dc5b6a0cb77ceec00451f753
adarshdhital007 <adarsh7466@gmail.com>
2023-05-17 19:39:25 +0530

    N

---
+++ Leetcode/2130.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+//template(adarshdhital007)
+#define ll long long
+#define ld long double
+#define ar array
+
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+
+#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define EACH(x, a) for (auto& x: a)
+
+template<class T> bool umin(T& a, const T& b) {
+    return b<a?a=b, 1:0;
+}
+
+template<class T> bool umax(T& a, const T& b) { 
+    return a<b?a=b, 1:0;
+}
+
+mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
+ll randint(ll a, ll b) {
+    return uniform_int_distribution<ll>(a, b)(mt_rng);
+}
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        int sum = INT_MIN;
+        vt<int> demo;
+        while (head != NULL) {
+            demo.push_back(head->val);
+            head = head->next;
+        }
+        int n = demo.size();
+        F_OR3(i,0,n/2) {
+            sum = max(sum, demo[i] + demo[n - 1 - i]);
+        }
+        return sum;
+    }
+};
+
+void solve() {
+   
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(3);
+
+    Solution solution;
+    int result = solution.pairSum(head);
+    cout << "Maximum twin sum: " << result << endl;
+
+
+    delete head->next->next->next;
+    delete head->next->next;
+    delete head->next;
+    delete head;
+}
+
+int main() {
+    solve();
+    return 0;
+}
