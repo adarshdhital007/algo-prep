ed0307da15e64f170dd8b923e26e678dfdf63a64
adarshdhital007 <adarsh7466@gmail.com>
2023-05-19 01:41:14 +0530

    Update template.cpp

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,47 +1,130 @@
-#include<bits/stdc++.h>
-using namespace std;
-//template(adarshdhital007)
-#define ll long long
-#define ld long double
-#define ar array
-
-#define vt vector
-#define pb push_back
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
-
-
-#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define EACH(x, a) for (auto& x: a)
-
-template<class T> bool umin(T& a, const T& b) {
-return b<a?a=b, 1:0;
-}
-
-template<class T> bool umax(T& a, const T& b) { 
-return a<b?a=b, 1:0;
-}
-
-mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
-ll randint(ll a, ll b) {
-    return uniform_int_distribution<ll>(a, b)(mt_rng);
-}
+#include <bits/stdc++.h>
+    using namespace std;
+    /*adarshdhital007*/
+    #define ll long long
+    #define ld long double
+    #define ar array
+    #define vt vector
+    #define pb push_back
+    #define all(c) (c).begin(), (c).end()
+    #define sz(x) (int)(x).size()
      
-void solve() {
-    
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
+    #define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+    #define F_OR1(e) F_OR(i, 0, e, 1)
+    #define F_OR2(i, e) F_OR(i, 0, e, 1)
+    #define F_OR3(i, b, e) F_OR(i, b, e, 1)
+    #define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+    #define EACH(x, a) for (auto& x: a)
+     
+    template<class T> bool amin(T& a, const T& b) {
+    	return b<a?a=b, 1:0;
     }
-    return 0;
-}
+    template<class T> bool amax(T& a, const T& b) { 
+    	return a<b?a=b, 1:0;
+    } 
+     
+    ll FT(function<bool(ll)> f, ll lb, ll rb) {
+    	while(lb<rb) {
+    		ll mb=(lb+rb)/2;
+    		f(mb)?rb=mb:lb=mb+1; 
+    	} 
+    	return lb;
+    }
+    ll LT(function<bool(ll)> f, ll lb, ll rb) {
+    	while(lb<rb) {
+    		ll mb=(lb+rb+1)/2;
+    		f(mb)?lb=mb:rb=mb-1; 
+    	} 
+    	return lb;
+    }
+     
+    template<class A> void rd(vt<A>& v);
+    template<class A, size_t S> void rd(ar<A, S>& a);
+    template<class T> void rd(T& x) {
+    	cin >> x;
+    }
+    void rd(double& d) {
+    	string t;
+    	rd(t);
+    	d=stod(t);
+    }
+    void rd(long double& d) {
+    	string t;
+    	rd(t);
+    	d=stold(t);
+    }
+    template<class H, class... T> void rd(H& h, T&... t) {
+    	rd(h);
+    	rd(t...);
+    }
+    template<class A> void rd(vt<A>& x) {
+    	EACH(a, x)
+    		rd(a);
+    }
+    template<class A, size_t S> void rd(array<A, S>& x) {
+    	EACH(a, x)
+    		rd(a);
+    }
+     
+    string to_string(char c) {
+    	return string(1, c);
+    }
+    string to_string(bool b) {
+    	return b?"true":"false";
+    }
+    string to_string(const char* s) {
+    	return string(s);
+    }
+    string to_string(string s) {
+    	return s;
+    }
+    string to_string(vt<bool> v) {
+    	string res;
+    	FOR(sz(v))
+    		res+=char('0'+v[i]);
+    	return res;
+    }
+     
+    template<size_t S> string to_string(bitset<S> b) {
+      string res;
+    	FOR(S)
+    		res+=char('0'+b[i]);
+    	return res;
+    }
+    template<class T> string to_string(T v) {
+        bool f=1;
+        string res;
+        EACH(x, v) {
+    		if(!f)
+    			res+=' ';
+    		f=0;
+    		res+=to_string(x);
+    	}
+        return res;
+    }
+     
+    template<class A> void wr(A x) {
+    	cout << to_string(x);
+    }
+    template<class H, class... T> void wr(const H& h, const T&... t) { 
+    	wr(h);
+    	wr(t...);
+    }
+    void print() {
+    	wr("\n");
+    }
+    template<class H, class... T> void print(const H& h, const T&... t) { 
+    	wr(h);
+    	if(sizeof...(t))
+    		wr(' ');
+    	print(t...);
+    }
+ 
+    void solve(){
+
+    }
+     
+    int main(){
+        solve();
+        return 0;
+    }
