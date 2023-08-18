1bda59de1d5dfe973ff49fdde2b97c0b97b4932d
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-08-18 17:24:25 +0530

    Update ChangeMoney.cpp

--- DS-Coursera/AlgorithmicToolbox/Week3/ChangeMoney.cpp
+++ DS-Coursera/AlgorithmicToolbox/Week3/ChangeMoney.cpp
@@ -1,7 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int get_change(int m){
+    int n=m/10;
+    m%=10;
+    n+=m/5;
+    m%=5;
+    n+=m;
+    return n;
+}
 
 int main(){
+    int m;
+    cin>>m;
+    cout<<get_change(m)<<'\n';
     return 0;
 }
