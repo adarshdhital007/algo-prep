11d915e81d085c686dd8b9ecd652397f568cdb0f
Adarsh Dhital Khatri <90372904+adarshdhital007@users.noreply.github.com>
2025-05-09 17:52:46 +0545

    Time: 0 ms (100.00%) | Memory: 18 MB (8.55%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isSafe(int i, int j, int n, int m) {
-        return (i < n && i >= 0 && j >=0 && j < m);
+        return (i >= 0 && i < n && j >= 0 && j < m);
     }
+    int orangesRotting(vector<vector<int>>& mat) {
 
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
         queue<vector<int>> q;
-        vector<vector<int>> directions = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
+                if (mat[i][j] == 2)
                     q.push({i, j});
-                }
             }
         }
 
@@ -24,33 +25,30 @@ public:
             elapsedTime++;
 
             int len = q.size();
-
             while (len--) {
                 vector<int> curr = q.front();
                 q.pop();
+
                 int i = curr[0];
                 int j = curr[1];
 
-                for (auto dir : directions) {
+                for (auto& dir : directions) {
                     int x = i + dir[0];
                     int y = j + dir[1];
 
-                    if (isSafe(x, y, n, m) && grid[x][y] == 1) {
-                        grid[x][y] = 2;
+                    if (isSafe(x, y, n, m) && mat[x][y] == 1) {
+                        mat[x][y] = 2;
                         q.push({x, y});
                     }
                 }
             }
         }
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                  return -1;
-                }
+                if(mat[i][j] == 1)
+                return -1;
             }
         }
-
         return max(0, elapsedTime - 1);
     }
 };
